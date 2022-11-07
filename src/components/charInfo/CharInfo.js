dd5c8fa55e97ccdfecf23f4fd33cc8b46b5d25da
BalashovaMa <balashova.m.nd@gmail.com>
2022-11-07 13:59:22 +0200

    feat: add ErrorBoundary

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-//import Skeleton from '../skeleton/Skeleton';
+import Skeleton from '../skeleton/Skeleton';
 
 
 import './charInfo.scss';
@@ -65,14 +65,14 @@ class CharInfo extends Component {
     render() {
         const { char, loading, error } = this.state;
 
-        //const skeleton = char || loading || error ? null : <Skeleton/>
+        const skeleton = char || loading || error ? null : <Skeleton/>
         const errorMessage = error ? <ErrorMessage /> : null;
         const spinner = loading ? <Spinner /> : null;
         const content = !(loading || error || !char) ? <View char={char} /> : null;
 
         return (
             <div className="char__info">
-                {/* {skeleton} */}
+                {skeleton}
                 {spinner}
                 {errorMessage}
                 {content}
@@ -84,10 +84,14 @@ class CharInfo extends Component {
 const View = ({ char }) => {
 
     const { name, description, thumbnail, homepage, comics, wiki } = char;
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
     return (
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name} />
+                <img src={thumbnail} alt={name} style={imgStyle}/>
                 <div>
                     <div className="char__info-name">{name}</div>
                     <div className="char__btns">
@@ -105,9 +109,10 @@ const View = ({ char }) => {
             </div>
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
-
+                {comics.length>0 ? null : 'There is no comics with this character'}
                 {
                     comics.map((item, i) => {
+                        if (i>10) return;
                         return (
                             <li key={i} className="char__comics-item">
                                 {item.name}
