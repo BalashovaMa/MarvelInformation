51d4605887a4ddb3220b060e0d53d4d16ed88b0d
BalashovaMa <balashova.m.nd@gmail.com>
2022-12-13 13:37:22 +0200

    feat: add SingleComicPage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppHeader from "../appHeader/AppHeader";
 import ComicsPage from '../pages/ComicsPage';
 import MainPage from '../pages/MainPage';
+import Page404 from '../pages/404';
+import SingleComicPage from '../pages/SingleComicPage';
 
 
 
@@ -21,6 +23,8 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage />} />
                         <Route path="/comics" element={<ComicsPage />} />
+                        <Route path="/comics/:comicId" element={<SingleComicPage/>} />
+                        <Route path="*" element={<Page404/>} />
                     </Routes>
                 </main>
             </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -35,7 +35,7 @@ const useMarvelService = () => {
 
     const getComics = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
-        return _transformCharacter(res.data.results[0]);
+        return _transformComics(res.data.results[0]);
     }
    
     const _transformCharacter = (char) => {
