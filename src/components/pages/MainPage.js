c795ec09872786950ea8d919d923e4be8cb6565e
BalashovaMa <balashova.m.nd@gmail.com>
2023-02-07 09:44:43 +0200

    add helmet

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { Helmet } from "react-helmet";
 
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from '../../resources/img/vision.png';
+import CharSearchForm from "../charSearchForm/CharSearchForm";
 
 const MainPage = () => {
 
@@ -17,10 +19,22 @@ const MainPage = () => {
 
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Marvel information portal"
+                />
+
+                <title>Marvel Information portal</title>
+            </Helmet>
             <ErrorBoundary><RandomChar /></ErrorBoundary>
             <div className="char__content">
                 <ErrorBoundary><CharList onCharSelected={onCharSelected} /></ErrorBoundary>
-                <ErrorBoundary><CharInfo charId={selectedChar} /></ErrorBoundary>
+                <div>
+                    <ErrorBoundary><CharInfo charId={selectedChar} /></ErrorBoundary>
+                    <ErrorBoundary><CharSearchForm /></ErrorBoundary>
+                </div>
+
             </div>
             <img className="bg-decoration" src={decoration} alt="vision" />
         </>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -7,20 +7,15 @@ const useMarvelService = () => {
     const _apiKey = 'apikey=d53bee0b54871c02144f6a81abbfa320';
     const _baseOffset = 0;
 
-/*     getResource = async (url) => {
-        let res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
-    } */
-
     const getAllCharecters = async (offset = _baseOffset) => {
         const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
         return res.data.results.map(_transformCharacter);
     }
+
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    } 
 
     const getCharecter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
@@ -57,10 +52,10 @@ const useMarvelService = () => {
                 thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
                 pageCount: comics.pageCount ? `${comics.pageCount} pages` : 'There is not information about number of pages',
                 language: comics.textObjects.language || 'en-us',
-                price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
+                price: comics.prices[0].price ? `${comics.prices[0].price}$` : 'not available'
             }
     }
-    return {loading, error, getAllCharecters, getCharecter, clearError, getAllComics, getComics};
+    return {loading, error, getAllCharecters, getCharecter, clearError, getAllComics, getComics, getCharacterByName};
 }
 
 export default useMarvelService;
